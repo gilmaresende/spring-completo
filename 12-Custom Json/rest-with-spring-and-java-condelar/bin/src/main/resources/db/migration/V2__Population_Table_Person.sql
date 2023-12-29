829a13c3022ba4376f3d0401c292fe17ec1a9ca1
Gilmar <gilmaresende@gmail.com>
2023-12-29 09:30:47 -0300

    IGNORE

--- 12-Custom Json/rest-with-spring-and-java-condelar/bin/src/main/resources/db/migration/V2__Population_Table_Person.sql
+++
@@ -1,2 +0,0 @@
-INSERT INTO person (id, address,first_name,gender,last_name) VALUES (1,'ana.laura@gmail.com','Ana','F','Laura');
-INSERT INTO person (id, address,first_name,gender,last_name) VALUES (2,'gilmar74796@gmail.com','Gilmar','M','Resende');
